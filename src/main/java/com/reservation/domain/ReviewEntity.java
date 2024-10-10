47988c15e1e0e41c96058490e11a3121156d34b8
hjkim <hjhjgudwls1022@gmail.com>
2024-10-10 18:47:08 +0900

    feat: ReviewEntity 생성

---
+++ src/main/java/com/reservation/domain/ReviewEntity.java
@@ -0,0 +1,42 @@
+package com.reservation.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReviewEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Size(max = 200, message = "내용은 200자를 초과할 수 없습니다.")
+    private String content;
+
+    @Column(nullable = false)
+    @Min(1)
+    @Max(5)
+    private int rating;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id", nullable = false)
+    private MemberEntity member;
+
+    @ManyToOne
+    @JoinColumn(name = "store_id", nullable = false)
+    private StoreEntity store;
+
+    @ManyToOne
+    @JoinColumn(name = "reservation_id", nullable = false)
+    private ReservationEntity reservation;
+}
