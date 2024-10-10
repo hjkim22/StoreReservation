5f4d919db8feb2fb5cf0dfe5d4f93fd16582cd6d
hjkim <hjhjgudwls1022@gmail.com>
2024-10-11 04:49:00 +0900

    feat: JWT 토큰 프로바이더 구현

--- src/main/java/com/reservation/security/TokenProvider.java
+++ src/main/java/com/reservation/security/TokenProvider.java
@@ -23,13 +23,19 @@ import java.util.List;
 public class TokenProvider {
 
     private static final String KEY_ROLES = "roles";
-    private static final long TOKEN_EXPIRE_TIME = 1000 * 60 * 60;
+    private static final long TOKEN_EXPIRE_TIME = 1000 * 60 * 60; // 1시간
 
     private final MemberService memberService;
 
     @Value("{spring.jwt.secret}")
     private String secretKey;
 
+    /**
+     * 주어진 사용자 이름과 역할 목록으로 JWT 토큰을 생성
+     * @param username 사용자 이름
+     * @param roles 사용자 역할 목록
+     * @return 생성된 JWT 토큰
+     */
     public String generateToken(String username, List<String> roles) {
         Claims claims = Jwts.claims().setSubject(username);
         claims.put(KEY_ROLES, roles);
@@ -45,15 +51,32 @@ public class TokenProvider {
                 .compact();
     }
 
+    /**
+     * JWT 토큰에서 사용자 인증 정보를 가져옴
+     * @param jwt JWT 토큰
+     * @return 인증 정보
+     */
     public Authentication getAuthentication(String jwt) {
+        // JWT 토큰에서 사용자 이름으로 UserDetails 로드
         UserDetails userDetails = this.memberService.loadUserByUsername(this.getUsername(jwt));
+        // UsernamePasswordAuthenticationToken 생성
         return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
     }
 
+    /**
+     * JWT 토큰에서 사용자 이름 추출
+     * @param token JWT 토큰
+     * @return 사용자 이름
+     */
     public String getUsername(String token) {
         return this.parseClaims(token).getSubject();
     }
 
+    /**
+     * JWT 토큰의 유효성 검사
+     * @param token JWT 토큰
+     * @return 유효성 검사 결과 (true: 유효, false: 유효하지 않음)
+     */
     public boolean validateToken(String token) {
         if (!StringUtils.hasText(token)) return false;
 
@@ -61,6 +84,12 @@ public class TokenProvider {
         return !claims.getExpiration().before(new Date());
     }
 
+    /**
+     * JWT 토큰에서 클레임 파싱
+     * @param token JWT 토큰
+     * @return 클레임
+     * @throws ExpiredJwtException 만료된 토큰일 경우 발생
+     */
     private Claims parseClaims(String token) {
         try {
             return Jwts.parser().setSigningKey(this.secretKey).parseClaimsJws(token).getBody();
